8052fa71c40a1ba81f9b51f59dba7f6805f5479a
Sergi Pérez Labernia <sergi.perez@hpcnow.com>
2020-03-31 18:03:40 +0200

    Avoid to assign public ip to nodes

--- roles/slurm/files/citc_oci.py
+++ roles/slurm/files/citc_oci.py
@@ -88,7 +88,7 @@ def create_node_config(oci_config, hostname: str, ip: Optional[str], nodespace: 
         image_id=image,
         display_name=hostname,
         hostname_label=hostname,
-        create_vnic_details=oci.core.models.CreateVnicDetails(private_ip=ip, subnet_id=subnet) if ip else None,
+        create_vnic_details=oci.core.models.CreateVnicDetails(private_ip=ip, subnet_id=subnet, assign_public_ip=False) if ip else None,
         metadata={
             "ssh_authorized_keys": ssh_keys,
             "user_data": user_data,
